0c5250883d32ec78c83cf86e4c1f68672235013b
Luis Guzman <luismguzman@gmail.com>
2025-02-08 20:31:32 -0400

    Prepare for Heroku deployment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-"console.log('Hello, world!');" 
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello, Heroku!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
